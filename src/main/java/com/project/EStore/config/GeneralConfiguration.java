816291358f406e39567d81e0fc33fd333119460b
Nikolay Daskalov <daskalovnikolaj@gmail.com>
2022-02-10 19:37:58 +0200

    Added security config file and added beans in general config file.

--- src/main/java/com/project/EStore/config/GeneralConfiguration.java
+++ src/main/java/com/project/EStore/config/GeneralConfiguration.java
@@ -1,8 +1,12 @@
 package com.project.EStore.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,5 +15,13 @@ public class GeneralConfiguration extends GlobalMethodSecurityConfiguration impl
     //TODO: implement CORS protection
 
 
-    
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }
