b4a761c19e83c5d9d1ccd50b154e77362aebbc95
Nikolay Daskalov <daskalovnikolaj@gmail.com>
2022-02-12 12:26:25 +0200

    Refactored app.yml and config file.

---
+++ src/main/java/com/project/EStore/TestController.java
@@ -0,0 +1,37 @@
+package com.project.EStore;
+
+import com.cloudinary.Cloudinary;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+@Controller
+public class TestController {
+
+    private final Cloudinary cloudinary;
+
+    public TestController(Cloudinary cloudinary) {
+        this.cloudinary = cloudinary;
+    }
+
+    @GetMapping("/web")
+    public String index() {
+        return "index";
+    }
+
+    @PostMapping("/webP")
+    public String postIndex(@RequestParam(name = "testFile") MultipartFile multipartFile) throws IOException {
+        File file = File.createTempFile("test", "test");
+        multipartFile.transferTo(file);
+        cloudinary.uploader().upload(file, Map.of());
+
+        System.out.println();
+        return "redirect:index";
+    }
+}
--- src/main/java/com/project/EStore/config/GeneralAppConfig.java
+++ src/main/java/com/project/EStore/config/GeneralAppConfig.java
@@ -1,5 +1,6 @@
 package com.project.EStore.config;
 
+import com.cloudinary.Cloudinary;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,11 +10,20 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Map;
+
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class GeneralAppConfig extends GlobalMethodSecurityConfiguration implements WebMvcConfigurer{
+public class GeneralAppConfig
+        extends GlobalMethodSecurityConfiguration
+        implements WebMvcConfigurer {
     //TODO: implement CORS protection
 
+    private final CloudinaryConfig config;
+
+    public GeneralAppConfig(CloudinaryConfig config) {
+        this.config = config;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -21,7 +31,17 @@ public class GeneralAppConfig extends GlobalMethodSecurityConfiguration implemen
     }
 
     @Bean
-    public ModelMapper modelMapper(){
+    public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+
+    @Bean
+    public Cloudinary cloudinary() {
+        return new Cloudinary(Map.of(
+                "cloud_name", config.getCloudName(),
+                "api_key", config.getApiKey(),
+                "api_secret", config.getApiSecret()
+        ));
     }
 }
