46f265f9b30697f1e62674f3178385c3c4c764fc
Nikolay Daskalov <daskalovnikolaj@gmail.com>
2022-02-23 10:33:00 +0200

    Optimized header img for smaller devices in index.html

---
+++ src/main/resources/static/js/events.js
@@ -0,0 +1,11 @@
+function changeImageHandler() {
+    const img = document.getElementById('header-img');
+    if (img.width < 500) {
+        img.src = '/images/header-logo-small.jpg';
+    } else {
+        img.src = '/images/header-logo.jpg';
+    }
+}
+
+window.addEventListener('load', changeImageHandler);
+window.addEventListener('resize', changeImageHandler);
