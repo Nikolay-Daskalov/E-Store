4cfe956b1b85b9de0903610f905a81e596edf389
Nikolay Daskalov <daskalovnikolaj@gmail.com>
2022-04-28 20:31:06 +0300

    Refactored ProductController and added productDetails.html

--- src/main/java/com/project/EStore/web/controller/ProductController.java
+++ src/main/java/com/project/EStore/web/controller/ProductController.java
@@ -11,11 +11,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.HashSet;
-import java.util.LinkedHashMap;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -36,7 +36,7 @@ public class ProductController {
     @GetMapping("fitness")
     public String getFitnessView(
             @RequestParam(name = "brands", required = false) Set<String> brands,
-            @RequestParam(name = "types", required = false) Set<ProductTypeEnum> types,
+            @RequestParam(name = "types", required = false) Set<String> types,
             @RequestParam(name = "lowestPrice", defaultValue = "0") String lowestPrice,
             @RequestParam(name = "highestPrice", defaultValue = "100") String highestPrice,
             @RequestParam(name = "pageNumber", defaultValue = "0") String pageNumber,
@@ -50,8 +50,65 @@ public class ProductController {
         return "product";
     }
 
+    @GetMapping("fitness/details/{id}")
+    public String getFitnessDetailsView(@PathVariable("id") String productId) {
+        return "productDetails";
+    }
+
+
+    @GetMapping("hiking")
+    public String getHikingView(
+            @RequestParam(name = "brands", required = false) Set<String> brands,
+            @RequestParam(name = "types", required = false) Set<String> types,
+            @RequestParam(name = "lowestPrice", defaultValue = "0") String lowestPrice,
+            @RequestParam(name = "highestPrice", defaultValue = "100") String highestPrice,
+            @RequestParam(name = "pageNumber", defaultValue = "0") String pageNumber,
+            Model model) {
+
+        findAllProductsByCriteria(
+                brands, types, lowestPrice, highestPrice, pageNumber, PAGE_SIZE,
+                ProductCategoryEnum.HIKING, model
+        );
+
+        return "product";
+    }
+
+    @GetMapping("running")
+    public String getRunningView(
+            @RequestParam(name = "brands", required = false) Set<String> brands,
+            @RequestParam(name = "types", required = false) Set<String> types,
+            @RequestParam(name = "lowestPrice", defaultValue = "0") String lowestPrice,
+            @RequestParam(name = "highestPrice", defaultValue = "100") String highestPrice,
+            @RequestParam(name = "pageNumber", defaultValue = "0") String pageNumber,
+            Model model) {
+
+        findAllProductsByCriteria(
+                brands, types, lowestPrice, highestPrice, pageNumber, PAGE_SIZE,
+                ProductCategoryEnum.RUNNING, model
+        );
+
+        return "product";
+    }
+
+    @GetMapping("football")
+    public String getFootballView(
+            @RequestParam(name = "brands", required = false) Set<String> brands,
+            @RequestParam(name = "types", required = false) Set<String> types,
+            @RequestParam(name = "lowestPrice", defaultValue = "0") String lowestPrice,
+            @RequestParam(name = "highestPrice", defaultValue = "100") String highestPrice,
+            @RequestParam(name = "pageNumber", defaultValue = "0") String pageNumber,
+            Model model) {
+
+        findAllProductsByCriteria(
+                brands, types, lowestPrice, highestPrice, pageNumber, PAGE_SIZE,
+                ProductCategoryEnum.FOOTBALL, model
+        );
+
+        return "product";
+    }
+
     private void findAllProductsByCriteria(
-            Set<String> brands, Set<ProductTypeEnum> types,
+            Set<String> brands, Set<String> types,
             String lowestPrice, String highestPrice, String pageNumber, int pageSize,
             ProductCategoryEnum productCategory, Model model) {
 
@@ -64,7 +121,7 @@ public class ProductController {
             highestPriceConverted = Integer.parseInt(highestPrice);
             pageNumberConverted = Integer.parseInt(pageNumber);
         } catch (NumberFormatException e) {
-            throw new ProductCriteriaException("Request type not valid");
+            throw new ProductCriteriaException("Price and page number not valid");
         }
 
         if (lowestPriceConverted < 0 || lowestPriceConverted > ProductService.HIGHEST_PRICE || lowestPriceConverted % 5 != 0) {
@@ -85,19 +142,24 @@ public class ProductController {
             }
         }
 
+        Set<ProductTypeEnum> typesConverted = null;
         Set<String> typeCheckboxesToCheck = new HashSet<>();
         if (types != null) {
-            if (types.isEmpty()) {
-                types = null;
-            } else {
-                typeCheckboxesToCheck.addAll(types.stream().map(Enum::toString).collect(Collectors.toSet()));
-                model.addAttribute("types", types.stream().map(Enum::toString).collect(Collectors.joining(",")));
+            if (!types.isEmpty()) {
+                try {
+                    typesConverted = types.stream().map(ProductTypeEnum::valueOf)
+                            .peek(productEnum -> typeCheckboxesToCheck.add(productEnum.toString())).collect(Collectors.toSet());
+                    model.addAttribute("types", String.join(",", types));
+                } catch (IllegalArgumentException e) {
+                    throw new ProductCriteriaException("Type not valid");
+                }
             }
         }
 
         Page<ProductServiceModel> page = this.productService.findAllByBrandAndTypeAndCategoryAndPriceBetween(
-                brands, types, productCategory, lowestPriceConverted, highestPriceConverted, pageNumberConverted, pageSize
+                brands, typesConverted, productCategory, lowestPriceConverted, highestPriceConverted, pageNumberConverted, pageSize
         );
+
         model.addAttribute("allBrands", this.productService.getAllBrandsByCategory(productCategory));
         model.addAttribute("brandCheckboxesToCheck", brandCheckboxesToCheck);
         model.addAttribute("typeCheckboxesToCheck", typeCheckboxesToCheck);
@@ -107,24 +169,6 @@ public class ProductController {
         model.addAttribute("allProductCards", page.map(product -> this.modelMapper.map(product, ProductCardViewModel.class)).getContent());
         model.addAttribute("totalPages", page.getTotalPages());
         model.addAttribute("pageNumber", page.getPageable().getPageNumber());
-    }
-
-
-    @GetMapping("hiking")
-    public String getHikingView(
-            @RequestParam(name = "brands", required = false) Set<String> brands,
-            @RequestParam(name = "types", required = false) Set<ProductTypeEnum> types,
-            @RequestParam(name = "lowestPrice", defaultValue = "0") String lowestPrice,
-            @RequestParam(name = "highestPrice", defaultValue = "100") String highestPrice,
-            @RequestParam(name = "pageNumber", defaultValue = "0") String pageNumber,
-            Model model) {
-
-        findAllProductsByCriteria(
-                brands, types, lowestPrice, highestPrice, pageNumber, PAGE_SIZE,
-                ProductCategoryEnum.HIKING, model
-        );
-
-        return "product";
     }
 }
 
--- src/main/resources/static/js/productEvents.js
+++ src/main/resources/static/js/productEvents.js
@@ -1,40 +1,43 @@
-const currenTarget = window;
-const footerElement = document.getElementsByTagName('footer')[0];
-const bodyElement = document.getElementsByTagName('body')[0];
-if (currenTarget.outerHeight > bodyElement.clientHeight) {
-    footerElement.classList.add('position-fixed', 'w-100', 'bottom-0');
-}
-currenTarget.addEventListener('resize', (e) => {
+window.addEventListener("load", (e) => {
+    const currenTarget = e.currentTarget;
+    const footerElement = document.getElementsByTagName('footer')[0];
+    const bodyElement = document.getElementsByTagName('body')[0];
     if (currenTarget.outerHeight > bodyElement.clientHeight) {
         footerElement.classList.add('position-fixed', 'w-100', 'bottom-0');
-    } else {
-        footerElement.classList.remove('position-fixed', 'w-100', 'bottom-0');
     }
-})
-
-function changeRangeLabelText(currentTarget, pes, text) {
-    pes.textContent = text + currentTarget.value;
-}
-
-const lowestCostRangeInputElement = document.getElementById('lowestCostRange');
-lowestCostRangeInputElement.addEventListener('input', (e) => {
-    changeRangeLabelText(e.currentTarget, e.currentTarget.previousElementSibling,
-        'Lowest price: ');
-});
-
-const highestCostRangeInputElement = document.getElementById('highestCostRange');
-highestCostRangeInputElement.addEventListener('input', (e) => {
-    changeRangeLabelText(e.currentTarget, e.currentTarget.previousElementSibling,
-        'Highest price: ');
-});
-
-const accordionButton = document.getElementsByClassName('accordion-button')[0];
-accordionButton.addEventListener('click', (e) => {
-    setTimeout(() => {
+    currenTarget.addEventListener('resize', (e) => {
         if (currenTarget.outerHeight > bodyElement.clientHeight) {
             footerElement.classList.add('position-fixed', 'w-100', 'bottom-0');
         } else {
             footerElement.classList.remove('position-fixed', 'w-100', 'bottom-0');
         }
-    }, 250);
+    })
+
+    function changeRangeLabelText(currentTarget, pes, text) {
+        pes.textContent = text + currentTarget.value;
+    }
+
+    const lowestCostRangeInputElement = document.getElementById('lowestCostRange');
+    lowestCostRangeInputElement.addEventListener('input', (e) => {
+        changeRangeLabelText(e.currentTarget, e.currentTarget.previousElementSibling,
+            'Lowest price: ');
+    });
+
+    const highestCostRangeInputElement = document.getElementById('highestCostRange');
+    highestCostRangeInputElement.addEventListener('input', (e) => {
+        changeRangeLabelText(e.currentTarget, e.currentTarget.previousElementSibling,
+            'Highest price: ');
+    });
+
+    const accordionButton = document.getElementsByClassName('accordion-button')[0];
+    accordionButton.addEventListener('click', (e) => {
+        setTimeout(() => {
+            if (currenTarget.outerHeight > bodyElement.clientHeight) {
+                footerElement.classList.add('position-fixed', 'w-100', 'bottom-0');
+            } else {
+                footerElement.classList.remove('position-fixed', 'w-100', 'bottom-0');
+            }
+        }, 250);
+
+    });
 });
