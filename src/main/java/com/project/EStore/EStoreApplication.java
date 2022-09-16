51f62e74b27d9e01813192d7ed3bc25e9822c0f0
Nikolay Daskalov <daskalovnikolaj@gmail.com>
2022-09-16 13:41:34 +0300

    Refactored Main Class.

--- src/main/java/com/project/EStore/EStoreApplication.java
+++ src/main/java/com/project/EStore/EStoreApplication.java
@@ -2,12 +2,8 @@ package com.project.EStore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
-@EnableCaching
 public class EStoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EStoreApplication.class, args);
